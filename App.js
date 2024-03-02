affa641380829ed807373f51a1987f7434e8031c
MuhammadAhmed2 <exact.ahmed@gmail.com>
2024-03-02 14:39:05 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+const apiKey='eee0f7ffadb7edd27e91189100b8f87b';
+const apiUrl='https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name+", " + data.sys.country;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " Kmph";
+}
+searchBtn.addEventListener("click" , ()=>{
+    checkWeather(searchBox.value);
+})
